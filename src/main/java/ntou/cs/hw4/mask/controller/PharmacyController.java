52cb2b3c2eb506c9ff79faa52f2be172d0332403
andy <00557106@email.ntou.edu.tw>
2021-06-05 13:42:52 +0800

    sast test

--- src/main/java/ntou/cs/hw4/mask/controller/PharmacyController.java
+++ src/main/java/ntou/cs/hw4/mask/controller/PharmacyController.java
@@ -33,8 +33,8 @@ public class PharmacyController {
 	
 
 	@PostMapping("/SecutiryTest")
-	public void SecutiryTest(@RequestBody String note){
-		Note myNote = new Note(note);
+	public void SecutiryTest(@RequestBody Note note){
+		// Note myNote = new Note(note);
 	}
 
 
